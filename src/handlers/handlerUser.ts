b6e1ce7ff9b971084d49905653c86784be3531e1
javiers78889 <javiers78889@gmail.com>
2024-11-22 01:10:50 -0500

    aprendizaje

--- src/handlers/handlerUser.ts
+++ src/handlers/handlerUser.ts
@@ -46,28 +46,15 @@ export const getUsers = async (req, res) => {
 
 export const updateUser = async (req, res, next) => {
 
-    const { id, email, usuario, password, role, plan } = req.body;
+    const { id, email, usuario, role, plan } = req.body;
 
     try {
         const editar = await Users.findOne({ where: { id } })
 
-        const verify = await bcrypt.compare(password.trim(), editar.dataValues.password)
-        console.log(editar.dataValues.password)
-        console.log(req.body)
 
-        if (verify) {
 
-            res.json({mensaje:'Contasena igual'})
-
-        }
-        else {
-            console.log('no era igual')
-            const obj = { email, usuario, role, plan }
-            await editar.update(obj)
-            res.status(201).json({ mensaje: 'Actualizado' })
-
-        }
-
+        await editar.update(req.body)
+        res.status(201).json({ mensaje: 'Actualizado' })
 
 
     } catch (error) {
